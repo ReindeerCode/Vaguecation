9d50e224262a4cb8ea3207c62af20c65dc25d429
MJRudolph <ReindeerCode@gmail.com>
2021-03-04 16:17:10 -0500

    nav, footer, scrollbar updates

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from "./logo.svg";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import BlueNav from "./Components/Navbar";
+import Footer from "./Components/Footer";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <BlueNav />
+        <Switch>
+          {/* <Route path="/" exact component={Landing} />
+        <Route path="/what" component={What} />
+        <Route path="/randomize" component={Randomize} />
+        <Route path="/contact" component={Contact} /> */}
+        </Switch>
+        <Footer />
+      </Router>
+    </>
   );
 }
 
---
+++ src/Components/Footer.js
@@ -0,0 +1,94 @@
+import React from "react";
+import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./Footer.css";
+
+import {
+  faFacebook,
+  faGithub,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
+
+const openSiteNewTab = (url) => {
+  const newWindow = window.open(url, "_blank", "noopener,noreferrer");
+  if (newWindow) newWindow.opener = null;
+};
+
+const Footer = () => {
+  return (
+    <MDBFooter
+      style={{ backgroundColor: "rgb(0,123,255)" }}
+      className=" text-center sticky-bottom font-small pt-4 "
+    >
+      <MDBContainer fluid className="text-center ">
+        <MDBRow classname="d-flex justify-content-center">
+          <MDBCol md="6">
+            <h5 className="title">Thanks for coming!</h5>
+            <p>
+              I am extremely excited to take the leap into programming and I
+              love talking about it. Please feel free to reach out to me anytime
+              through any of these icon links.
+            </p>
+          </MDBCol>
+          <MDBCol md="2" sm="4">
+            <div className="list-unstyled fontA-icons">
+              <href
+                onClick={() =>
+                  openSiteNewTab("https://github.com/ReindeerCode")
+                }
+              >
+                <FontAwesomeIcon
+                  className="fontA-icons"
+                  icon={faGithub}
+                  size="4x"
+                />
+              </href>
+            </div>
+          </MDBCol>
+          <MDBCol md="2" sm="4">
+            <div className="list-unstyled fontA-icons">
+              <href
+                onClick={() =>
+                  openSiteNewTab(
+                    "https://www.linkedin.com/in/a-michael-rudolph/"
+                  )
+                }
+              >
+                <FontAwesomeIcon
+                  className="fontA-icons"
+                  icon={faLinkedin}
+                  size="4x"
+                />
+              </href>
+            </div>
+          </MDBCol>
+          <MDBCol md="2" sm="4">
+            <div className="list-unstyled fontA-icons">
+              <href
+                onClick={() =>
+                  openSiteNewTab(
+                    "https://www.facebook.com/michael.rudolph.3939/"
+                  )
+                }
+              >
+                <FontAwesomeIcon
+                  className="fontA-icons"
+                  icon={faFacebook}
+                  size="4x"
+                />
+              </href>
+            </div>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+      <div className="footer-copyright text-center py-3">
+        <MDBContainer fluid>
+          &copy; {new Date().getFullYear()} Copyright:{" "}
+          <a href="https://github.com/ReindeerCode"> Michael Rudolph </a>
+        </MDBContainer>
+      </div>
+    </MDBFooter>
+  );
+};
+
+export default Footer;

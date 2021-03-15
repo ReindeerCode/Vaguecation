b380a4b872b8eafaa9f45c79b6de9197af452cf9
MJRudolph <ReindeerCode@gmail.com>
2021-03-15 19:41:15 -0400

    ran build and removed comments on server.js

--- server.js
+++ server.js
@@ -6,19 +6,15 @@ const routes = require("./routes");
 const PORT = process.env.PORT || 3001;
 const app = express();
 dbConnect();
-// Define middleware here
-app.use(express.urlencoded({ extended: false }));
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
-// Serve up static assets (usually on heroku)
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Define API routes here
-
-// Send every other request to the React app
-// Define any API routes before this runs
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });

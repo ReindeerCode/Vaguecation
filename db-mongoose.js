570b0d91cfe0a734a36e2c06c1042cd48fdca3bc
MJRudolph <ReindeerCode@gmail.com>
2021-03-15 18:18:55 -0400

    adding atlas process.env.MONGODB_URI for Heroku

--- db-mongoose.js
+++ db-mongoose.js
@@ -1,20 +1,12 @@
+const mongoose = require("mongoose");
 
-const mongoose = require('mongoose');
-
-// mongoose.connect(
-//     process.env.MONGODB_URI || "mongodb://localhost:27017/VaguecationDB",
-//     {useNewUrlParser:true}
-//   );
-
-function dbConnect(url = "mongodb://localhost:27017/VaguecationDB"){
-    return mongoose.connect(url,{ useUnifiedTopology: true })
+function dbConnect(url = "mongodb://localhost:27017/VaguecationDB") {
+  return mongoose
+    .connect(process.env.MONGODB_URI || url, { useUnifiedTopology: true })
     .then(() => console.log(`Mongoose is connect to  ${url}`))
-    .catch(err => 
-        console.error('Mongoose failed to connect'))
-
+    .catch((err) => console.error("Mongoose failed to connect"));
 }
-
 
 module.exports = {
-    dbConnect,
-}
+  dbConnect,
+};

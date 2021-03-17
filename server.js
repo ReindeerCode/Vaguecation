e12a45e629e126a24a9a54650a5eea7b5234deba
MJRudolph <ReindeerCode@gmail.com>
2021-03-16 20:09:01 -0400

    deploy debugging

--- server.js
+++ server.js
@@ -9,15 +9,11 @@ dbConnect();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(routes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+app.use(routes);
 
 dbConnect()
   .then(() => {

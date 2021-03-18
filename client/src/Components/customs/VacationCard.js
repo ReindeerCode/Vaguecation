75d03cc77cdb9cc0bc21b2ef8a40f98bd4e24f15
MJRudolph <ReindeerCode@gmail.com>
2021-03-18 19:57:25 -0400

    changed photos api call quiery from travel to vacation

--- client/src/Components/customs/VacationCard.js
+++ client/src/Components/customs/VacationCard.js
@@ -1,22 +1,9 @@
 import React from "react";
 import { Button, Card } from "react-bootstrap";
-import { HashLink as Link } from "react-router-hash-link";
-
-const scrollWithOffset = (el) => {
-  const yCoordinate = el.getBoundingClientRect().top + window.pageYOffset;
-  const yOffset = -110;
-  window.scrollTo({ top: yCoordinate + yOffset, behavior: "smooth" });
-};
 
 export const VacationCard = (props) => {
   return (
-    <Button
-      variant="outline-primary"
-      type="submit"
-      smooth={true}
-      to="#randomize"
-      scroll={(el) => scrollWithOffset(el)}
-    >
+    <Button variant="outline-primary" type="submit" to="#randomize">
       <Card>
         <Card.Img variant="top" src={props.src} />
       </Card>
